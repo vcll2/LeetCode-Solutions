b65989fb1363e4cef711cfb34cceda4ebbd01975
kidd <979429373@qq.com>
2019-04-30 12:34:11 +0800

    Add files via upload

---
+++ C++/0001 Two Sum.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> hashmap;
+        for(int i=0;i<nums.size();++i){
+            int gap = target - nums[i];
+            if(hashmap.find(gap)!=hashmap.end() && hashmap[gap]!=i) return{i,hashmap[gap]};
+            hashmap[nums[i]]=i;   // 注意位置
+        }
+        return {};
+    }
+};
